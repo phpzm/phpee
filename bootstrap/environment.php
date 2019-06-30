abbb7cf36264dd86fc47fdaf4a26afec8ffe2564
William C�rrea <wilcorrea@gmail.com>
2019-06-30 13:31:38 -0300

    [fix] Add Php namespace

--- bootstrap/environment.php
+++ bootstrap/environment.php
@@ -4,7 +4,7 @@
 error_reporting(E_ALL);
 
 // system settings
-ini_set('display_errors', env('ERRORS_DISPLAY', 'On'));
-ini_set('log_errors', env('ERRORS_LOG', 'On'));
-ini_set('track_errors', env('ERRORS_TRACK', 'Off'));
-ini_set('html_errors', env('ERRORS_HTML', 'Off'));
+ini_set('display_errors', Php\env('ERRORS_DISPLAY', 'On'));
+ini_set('log_errors', Php\env('ERRORS_LOG', 'On'));
+ini_set('track_errors', Php\env('ERRORS_TRACK', 'Off'));
+ini_set('html_errors', Php\env('ERRORS_HTML', 'Off'));
