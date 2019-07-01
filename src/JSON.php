140fc65ee53472940667899e1e30957a79468f1c
William C�rrea <wilcorrea@gmail.com>
2019-07-01 00:08:54 -0300

    Update JSON.php

--- src/JSON.php
+++ src/JSON.php
@@ -6,7 +6,7 @@ namespace Php;
  * Class Base64
  *
  * @method static string encode(mixed $value, int $options = 0, int $depth = 512)
- * @method static string decode(string $json, bool $assoc = false, int $depth = 512, int $options = 0)
+ * @method static mixed decode(string $json, bool $assoc = false, int $depth = 512, int $options = 0)
  */
 abstract class JSON extends Proxy
 {
